d81ead4a742e52dc995ef90c544aa35765628c55
Brennon Lima <130801020+BrennonMLima@users.noreply.github.com>
2024-09-03 16:12:08 -0300

    Add files via upload

--- target3/target3.py
+++ target3/target3.py
@@ -1,16 +1,16 @@
 import json
 
-def carregar_dados(faturamento):
-    with open(faturamento, 'r') as arquivo:
+def carregar_dados(dados):
+    with open(dados, 'r') as arquivo:
         dados = json.load(arquivo)
-    return dados['faturamento']
+    return dados
 
 def analisar_faturamento(faturamento_diario):
     valores = [item['valor'] for item in faturamento_diario if item['valor'] > 0]
 
     if not valores:
-        return None, None, 0 
-    
+        return None, None, 0
+
     menor_valor = min(valores)
     maior_valor = max(valores)
     media_mensal = sum(valores) / len(valores)
@@ -19,12 +19,12 @@ def analisar_faturamento(faturamento_diario):
     return menor_valor, maior_valor, dias_acima_media
 
 def main():
-    faturamento_diario = carregar_dados('faturamento.json')
+    faturamento_diario = carregar_dados('dados.json')
 
     menor_valor, maior_valor, dias_acima_media = analisar_faturamento(faturamento_diario)
 
-    print(f"Menor valor de faturamento: {menor_valor}")
-    print(f"Maior valor de faturamento: {maior_valor}")
+    print(f"Menor valor de faturamento: {menor_valor:.2f}")
+    print(f"Maior valor de faturamento: {maior_valor:.2f}")
     print(f"Número de dias com faturamento acima da média: {dias_acima_media}")
 
 if __name__ == "__main__":
